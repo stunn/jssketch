030cc17513fa396c75152a181907e2fd1567c2ea
Matt Lunn <matt@mattlunn.me.uk>
2013-01-31 21:29:50 +0000

    Adding the minification of CSS and JS assets into the build process. Restructured the boot process to support CLI mode as well, and a config file

--- config.js
+++
@@ -1,9 +0,0 @@
-var utils = require('utils');
-var config = utils.readJsonFileSync(__dirname + '/config/config.json');
-var defaults = {
-  base: '127.0.0.1',
-  jail: '127.0.0.1',
-  port: 3000
-};
-
-module.exports = utils.extend(defaults, config);
