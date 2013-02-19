ae77158e06d75a128c96370a6a28e761e7874d37
Matt Lunn <matt@mattlunn.me.uk>
2013-02-19 11:01:04 +0000

    Working on #6 by removing Object.defineProperty dependency on Model.js.
    Set and get operations are now done the set() and get() methods
    respectively. update(hash) has been replaced by set(hash). Models now
    support events by using on(event, filter, callback) and trigger(event,
    filter). Because Handlebars does not let you use methods in the views,
    properties must be retrieved via the .properties[key] property rather
    than .get(key) method. Collections still exist directly on the Model
    instance (version.library etc).

--- shared/models/model.js
+++ shared/models/model.js
@@ -1,4 +1,15 @@
 define(['models/collection'], function (Collection) {
+
+  var defineProperty = (function () {
+    if (Object.defineProperty) {
+      return Object.defineProperty.bind(Object);
+    } else {
+      return function (obj, key, descriptor) {
+        obj[key] = descriptor.value;
+      };
+    }
+  }());
+
   /*
    * This is the Constructor that all Models will extend. Basically, the inheritance
    * chain will be:
@@ -10,59 +21,15 @@ define(['models/collection'], function (Collection) {
   }
 
   /**
-   * Updates multiple properties of the model at once, by passing a map of
-   * attributes: values as the first parameter. Which of the object keys are applied
-   * to the model can be specified using the optional second and third parameters.
+   * Validates the Model instance. If a profile is provided as the first
+   * parameter the properties are validated according to that profile. If no
+   * profile is provided, the properties are validated according to the options
+   * passed when the model was defined.
    *
-   * @param obj: A map of property: values to update
-   * @param which: An optional array of properties which will be taken from obj.
-   *        if this is left unspecified, only the properties which are updateable
-   *        will be updated.
-   * @param exclusive: By default, only the properties specified in "which" will be
-   *        set. Set exclusive to true to set only the properties which do not appear
-   *        in which.
-   * @return self
-   */
-  Base.prototype.update = function (obj, which, exclusive) {
-    var isAccepted = (function () {
-      if (arguments.length > 1) {
-        if (which instanceof Array) {
-          return function (key) {
-            var exists = which.indexOf(key) >= 0;
-
-            return exclusive ? !exists : exists;
-          };
-        } else {
-          throw new Error('update() expects 2nd parameter to be an array');
-        }
-      } else {
-        var props = this._model.properties;
-
-        return function (key) {
-          return props.hasOwnProperty(key) && props[key].updateable;
-        }
-      }
-    }).apply(this, arguments);
-
-    Object.keys(obj).forEach(function (key) {
-      if (this._properties.hasOwnProperty(key) && isAccepted(key)) {
-        this[key] = obj[key];
-      }
-    }, this);
-
-    return this;
-  }
-
-  /**
-   * Validates the Model instance. If a profile is provided as the first parameter
-   * the properties are validated according to that profile. If no profile is provided,
-   * the properties are validated according to the options passed when the model was
-   * defined.
-   *
-   * This is designed to be useful if you have different validation requirements for
-   * different parts of your application; e.g. you can validate a "Customer" according
-   * to one set of requirements in one part of your app, and by another set of requirements
-   * in another part.
+   * This is designed to be useful if you have different validation requirements
+   * for different parts of your application; e.g. you can validate a "Customer"
+   * according to one set of requirements in one part of your app, and by another
+   * set of requirements in another part.
    *
    * {
    *   propName: {
@@ -98,7 +65,7 @@ define(['models/collection'], function (Collection) {
       throw new Error('Profile passed to validate() must be an object');
     }
 
-    var properties = this._properties;
+    var properties = this.properties;
 
     function error(reason) {
       return reason;
@@ -151,16 +118,169 @@ define(['models/collection'], function (Collection) {
   }
 
   /**
+   * Updates either a single property on the object, or a hash of attributes.
+   *
+   * If a single property is to be updated, the first parameter should be the name
+   * of the attribute, and the second should be the new value. The third parameter
+   * defaults to false, and is whether the "change" event for the update should be
+   * supressed.
+   *
+   * In the second case, the first parameter should be the hash which maps properties
+   * to values. Properties which do not exist on this Model will be ignored. The
+   * second parameter is an optional array, which provides a "view" of which
+   * parameters from the given hash will be used. By default, all will be. The
+   * array overrrides any "updateable" property on the model. The final parameter
+   * is "silent", which behaves exactly as it does for the first use case.
+   *
+   * @param key: Name of value to update, or a hash of multiple values.
+   * @param value: The value to update to, or an array of which values to take
+   *        from the hash.
+   * @param silent: Whether the update will fire the "change" event on the model
+   * @return self
+   */
+  Base.prototype.set = function (key, value, silent) {
+    var descriptors = this._model.properties;
+
+    if (typeof key === "object") {
+      var hash = key;
+      var isSilent = silent || (typeof value === "boolean" ? value : false);
+      var isAccepted = (function () {
+        if (Array.isArray(value)) {
+          return function (key) {
+            return value.indexOf(key) >= 0;
+          };
+        } else {
+          var props = descriptors;
+
+          return function (key) {
+            return props.hasOwnProperty(key) && props[key].updateable;
+          }
+        }
+      }).apply(this, arguments);
+
+      Object.keys(hash).forEach(function (key) {
+        if (descriptors.hasOwnProperty(key) && isAccepted(key)) {
+          this.set(key, hash[key], true);
+
+          silent || this.trigger('change', key, true);
+        }
+      }, this);
+
+      silent && this.trigger('change', '*');
+    } else {
+      var descriptor = descriptors[key];
+
+      if (typeof descriptor === "undefined") {
+        throw new Error('Model does not have a property called ' + key);
+      }
+
+      if (typeof value === "undefined") {
+        value = descriptor.fallback;
+      }
+
+      if (this.properties[key] !== value) {
+        this.properties[key] = value;
+
+        silent || this.trigger('change', key);
+      }
+    }
+
+    return this;
+  };
+
+  /**
+   * Sets the given property to it's fallback value, or "undefined" if none was
+   * given.
+   *
+   * @param key: The property to unset
+   * @param silent: Whether the "change" event will fire on the model. Default false.
+   * @return self;
+   */
+  Base.prototype.unset = function (key, silent) {
+    this.set(key, undefined, silent);
+
+    return this;
+  };
+
+  /**
+   * Returns the current value of the "key" property, or "undefined" if the
+   * property does not exist.
+   *
+   * @param key: The parameter to retrieve
+   * @return value
+   */
+  Base.prototype.get = function (key) {
+    return this.properties[key];
+  };
+
+  /**
+   * Attaches an event handler for the "event" event, optionally filtered by the
+   * "filter" filter. In practise, the "event" is something generic like "change",
+   * and "filter" is something more specific such as the property the change
+   * event was fired on.
+   *
+   * @param event The name of the event we want to subscribe for
+   * @param filter Default '*' (i.e. all occurences of the event). Allows us to
+   *        optionally filter for only specific types of the event.
+   * @param callback The callback function. "this" will be the instance the event
+   *        occured on
+   * @return self
+   */
+  Base.prototype.on = function (event, filter, callback) {
+    if (typeof filter === "function") {
+      callback = filter;
+      filter = '*';
+    };
+
+    var events = this._events[event] = this._events[event] || {};
+
+    if (!events.hasOwnProperty(filter)) {
+      events[filter] = [];
+    }
+
+    events[filter].push(callback);
+
+    return this;
+  };
+
+  /**
+   * Triggers the "event" event on the Model Instance.
+   *
+   * @param event: The name of the event to trigger
+   * @param filter: An optional filter of the event (@see on). Default is '*'
+   * @param local: Whether the global '*' event will be supressed. Default is false
+   *        this is mainly used internally.
+   * @return self
+   */
+  Base.prototype.trigger = function (event, filter, local) {
+    var events = this._events[event] = this._events[event] || {};
+
+    filter = filter || '*';
+
+    if (events.hasOwnProperty(filter)) {
+      events[filter].forEach(function (callback) {
+        callback.apply(this);
+      }, this);
+    }
+
+    if (filter !== '*' && !local) {
+      this.trigger(event, '*');
+    }
+
+    return this;
+  };
+
+  /**
    * Returns the JSON representation of the object; which are it's properties.
    *
    * @see https://developer.mozilla.org/en/docs/JSON#toJSON()_method
    * @return Object which represents the JSON representation of this model instance.
    */
   Base.prototype.toJSON = function () {
-    // TODO: This should probably return a copy of the _properties;
-    // utils.extend({}, this._properties) would support this, but utils isn't currently
+    // TODO: This should probably return a copy of the properties;
+    // utils.extend({}, this.properties) would support this, but utils isn't currently
     // available as a AMD module.
-    return this._properties;
+    return this.properties;
   }
 
   /**
@@ -197,54 +317,48 @@ define(['models/collection'], function (Collection) {
    * @return A constructor for the model.
    */
   return function (model) {
-    if (typeof model.collections !== "object") {
-      model.collections = {};
+    if (typeof model !== "object" && !model.hasOwnProperty('properties')) {
+      throw new Error('Model must be provided with properties');
     }
 
     function Constructor(properties) {
-      // _properties is where the actual properties are stored; not on the Model
-      // itself.
-      Object.defineProperty(this, '_properties', {
+      var descriptors = this._model.properties;
+
+      switch (typeof properties) {
+        case "object":
+        break;
+        case "undefined":
+          properties = {};
+        break;
+        default:
+          throw new Error('Properties must be an object, or left undefined');
+      }
+
+      defineProperty(this, 'properties', {
         enumerable: false,
         value: {}
       });
 
-      // Define a setter and getter for each property. Note that no validation
-      // is done on the setting; that's in validate(). The only magic that set
-      // does it set the property to its default value, when it is set to undefined.
-      Object.keys(model.properties).forEach(function (key) {
-        Object.defineProperty(this, key, {
-          enumerable: true,
-          get: function () {
-            return this._properties[key];
-          },
-          set: function (val) {
-            if (typeof val === "undefined") {
-              val = model.properties[key].fallback;
-            }
+      defineProperty(this, '_events', {
+        enumerable: false,
+        value: {}
+      });
 
-            this._properties[key] = val;
-          }
-        });
-
-        this[key] = undefined;
+      Object.keys(descriptors).forEach(function (key) {
+        this.set(key, properties[key], true);
       }, this);
 
-      // Establish collections
-      Object.keys(model.collections).forEach(function (key) {
-        Object.defineProperty(this, key, {
-          value: new Collection(model.collections[key]),
-          writeable: false,
-          enumerable: false
-        });
-      }, this);
-
-      // Constructor accepts initial state of object; set the properties here.
-      if (typeof properties === "object") {
-        this.update(properties, [], true);
+      if (typeof model.collections === "object") {
+        Object.keys(model.collections).forEach(function (key) {
+          defineProperty(this, key, {
+            value: new Collection(model.collections[key]),
+            writeable: false,
+            enumerable: false
+          });
+        }, this);
       }
 
-      Object.seal(this);
+      (typeof Object.seal === "function") && Object.seal(this);
     }
 
     Constructor.prototype = new Base;
