48c3b12f91e4dd4f39c25f47f7decf9bbacbfdcf
Matt Lunn <matt@mattlunn.me.uk>
2013-03-19 17:18:48 +0000

    Further work to build process... CSS is now included in the build.
    Work to do includes respecting the bundle name, and the "js" setting for
    the bundle for script files, and tidying up the code (i.e. config'ing
    the folders etc.).

--- build.js
+++ build.js
@@ -1,27 +1,48 @@
-var requirejs = require('requirejs');
 var utils = require('utils');
+var bundles = utils.readJsonFileSync(__dirname + '/config/bundles');
 
-var jsModules = utils.readJsonFileSync(__dirname + '/config/js_modules');
-var config = utils.readJsonFileSync(__dirname + '/config/requirejs');
+// Minify JS
+(function  () {
+  var requirejs = require('requirejs');
+  var config = utils.readJsonFileSync(__dirname + '/config/requirejs');
 
-utils.extend(config, {
-  dir: "build",
-  preserveLicenseComments: false,
-  baseUrl: __dirname + "/public/js",
-  paths: {
-    lib: "../../shared/lib",
-    helpers: "../../shared/helpers",
-    models: "../../shared/models",
-  },
-  modules: jsModules.map(function (name) {
-    return {
-      name: name
-    };
-  })
-}, true);
+  utils.extend(config, {
+    appDir: __dirname + '/public',
+    baseUrl: 'js',
+    dir: 'build',
+    preserveLicenseComments: false,
+    paths: {
+      lib: '../../shared/lib',
+      helpers: '../../shared/helpers',
+      models: '../../shared/models',
+    },
+    modules: Object.keys(bundles).filter(function (name) {
+      return this[name].js;
+    }, bundles).map(function (name) {
+      return {
+        name: name
+      };
+    })
+  }, true);
 
-requirejs.optimize(config, function (buildResponse) {
-  console.log(buildResponse);
-}, function(err) {
-  console.log(err.toString());
-});
+  requirejs.optimize(config, function (buildResponse) {
+    console.log(buildResponse);
+  }, function(err) {
+    console.log(err.toString());
+  });
+}());
+
+// Minify CSS
+(function () {
+  var minifier = require('mini-fier');
+
+  Object.keys(bundles).forEach(function (name) {
+    var files = bundles[name].css || [];
+
+    minifier.css({
+      srcPath: __dirname + '/public/',
+      filesIn: files,
+      destination: __dirname + '/build/css/all.' + name + '.css'
+    });
+  });
+}());
