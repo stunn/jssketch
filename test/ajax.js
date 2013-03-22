a49b3e87f01be122b515babeb60b52942f9fedf9
Matt Lunn <matt@mattlunn.me.uk>
2013-03-22 15:47:53 +0000

    Fixing a few bugs with the AJAX mechanism.

---
+++ test/ajax.js
@@ -0,0 +1,46 @@
+var app = require('./support/server');
+
+describe('AJAX', function () {
+  var payload = {
+    test: 1
+  };
+  var id;
+
+  it('should create successfully', function (done) {
+    app.postBase('/ajax/create').send({
+      payload: JSON.stringify({
+        test: 1
+      }),
+
+      type: 'json'
+    }).expect(200).end(function (err, res) {
+      if (!err) {
+        var response;
+
+        try {
+          response = JSON.parse(res.res.text);
+        } catch (e) {};
+
+        require('should').exist(response);
+        response.should.have.property('success', true);
+        response.should.have.property('id');
+
+        id = response.id;
+      }
+
+      done(err);
+    });
+  });
+
+  it('should retrieve successfully', function (done) {
+    app.getJail('/ajax/' + id).expect(200).expect('Content-Type', 'application/json').end(function (err, res) {
+      if (!err) {
+        (function () {
+          payload.should.be.eql(JSON.parse(res.res.text));
+        }).should.not.throw();
+      }
+
+      done(err);
+    });
+  });
+});
---
+++ test/support/server.js
@@ -0,0 +1,23 @@
+var request = require('supertest');
+var app = require('../../lib/app')({
+  "base": "jssketch.local.mattlunn.me.uk",
+  "jail": "jssketch-other.local.mattlunn.me.uk",
+  "port": 3000,
+  "production": false
+});
+
+module.exports.getBase = function (path) {
+  return request(app.app).get(app.config.getBaseUrl(path)).set('Host', app.config.get('base'));
+}
+
+module.exports.getJail = function (path) {
+  return request(app.app).get(app.config.getJailUrl(path)).set('Host', app.config.get('jail'));
+}
+
+module.exports.postBase = function (path) {
+  return request(app.app).post(app.config.getBaseUrl(path)).set('Host', app.config.get('base'));
+}
+
+module.exports.postJail = function (path) {
+  return request(app.app).post(app.config.getJailUrl(path)).set('Host', app.config.get('jail'));
+}
