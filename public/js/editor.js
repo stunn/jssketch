aeac998ad5e3d33ddf660b53616cd820984be360
Matt <matt@mattlunn.me.uk>
2013-09-01 18:17:11 +0100

    Adding view source option. "editor.js" is getting unwieldly and could do with splitting up.
    ... but I'll save that until we move away from requirejs, and reorganise
    all the JS.

--- public/js/editor.js
+++ public/js/editor.js
@@ -9,10 +9,10 @@ function log(message)
 define(
   ['module', 'jquery', 'codemirror/lib/codemirror', 'application',
    'librarymanager/presenter', 'editor/manager', 'editor/tab',
-   'handlebars', 'codemirror/mode/css/css',
-   'codemirror/mode/javascript/javascript', 'codemirror/mode/xml/xml',
-   'codemirror/mode/htmlmixed/htmlmixed'],
-  function (module, jQuery, CodeMirror, Application, LibManagerPresenter, EditorManager, Tab)
+   'handlebars', 'models/sketch', 'models/revision', 'helpers/revision',
+   'codemirror/mode/css/css', 'codemirror/mode/javascript/javascript',
+   'codemirror/mode/xml/xml', 'codemirror/mode/htmlmixed/htmlmixed'],
+  function (module, jQuery, CodeMirror, Application, LibManagerPresenter, EditorManager, Tab, Handlebars, Sketch, Revision, revisionHelper)
   {
     var config = module.config();
 
@@ -72,12 +72,13 @@ define(
       Object.keys(editors).forEach(function (type) {
         var target = $('textarea[name="' + type + '"]');
         var instance = CodeMirror.fromTextArea(target[0], {
-          mode: editors[type]
+          mode: editors[type],
+          lineNumbers: true
         });
         var tab = new Tab({
           id: type,
-          template: $(instance.getWrapperElement()),
-          detachable: true
+          detachable: true,
+          template: $(instance.getWrapperElement())
         });
 
         instance.setSize(null, '100%');
@@ -88,14 +89,63 @@ define(
         editor.tabs.push(tab);
       });
 
+      (function () {
+        var sourceTemplate = Handlebars.compile($('#source-template').html().replace(/<\\\/script>/g, '</script>'));
+        var cm = new CodeMirror(document.body, {
+          mode: 'htmlmixed',
+          readOnly: 'nocursor',
+          lineNumbers: true
+        });
+
+        var tab = new Tab({
+          id: 'view source',
+          detachable: true,
+          template: cm.getWrapperElement()
+        });
+
+        cm.setSize(null, '100%');
+
+        function prep() {
+          var params = $('#the-form').serializeArray();
+          var ret = {};
+
+          params.forEach(function (obj) {
+            ret[obj.name] = obj.value;
+          });
+
+          return ret;
+        }
+
+        tab.on('show', function () {
+          var revision = new Revision();
+          var sketch = new Sketch();
+
+          revisionHelper.updateRevisionFromHash(revision, Application.doctypes, Application.dm, prep());
+
+          cm.refresh();
+          cm.setValue(sourceTemplate({
+            revision: revision,
+            sketch: sketch,
+            doctype: Application.doctypes[revision.get('doctype')]
+          }));
+        });
+
+        editor.tabs.push(tab);
+      }());
+
       editor.tabs.push(new Tab({
         id: 'result',
         template: $('#render'),
         detachable: false
       }));
 
-      editor.setActiveTab(0, 'javascript');
-      editor.setActiveTab(1, config.isNew ? 'html' : 'result');
+      if (config.isNew) {
+        editor.setActiveTab(0, 'javascript');
+        editor.setActiveTab(1, 'html');
+      } else {
+        editor.setActiveTab(0, 'view source');
+        editor.setActiveTab(1, 'result');
+      }
 
       $('#run_btn').on('click', function (e) {
         var $form = $('#the-form');
